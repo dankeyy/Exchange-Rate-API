5a4d1c5f89f558c4b992f6ab046cbcb5d8cdf2ec
dankeyy <dankeyy@protonmail.com>
2022-11-14 12:01:15 +0200

    init, demo seems to be working

---
+++ exchange_rates.py
@@ -0,0 +1,44 @@
+import sys
+import json
+import os
+from typing import Optional
+import requests
+
+
+def low_rates(prod=False) -> Optional[list]:
+    payload = {}
+    filtered = lambda payload: [name for name, rate in payload["rates"].items() if rate < 10]
+
+    if prod:
+        url = "https://api.apilayer.com/exchangerates_data/latest?"
+
+        key = os.getenv("EXCHANGERATES_APIKEY_PROD")
+        if key is None:
+            raise ValueError("Could not find key")
+
+        headers = {
+        "apikey": key
+        }
+
+        response = requests.request("GET", url, headers=headers)
+        payload = json.loads(response.text)
+        status_code = response.status_code
+        if status_code == 200:
+            return filtered(payload)
+
+    else:
+        with open("res.json") as f:
+            payload = json.load(f)
+        return filtered(payload)
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == "prod":
+        res = low_rates(prod=True)
+        # print(res)
+    elif sys.argv[1] == "dev":
+        res = low_rates(prod=False)
+        # print(res)
+    else:
+        print("bad argument, specify dev or prod")
+        exit(1)
